90c459c57a09d85680b5a344267518e46764d9ad
ANIRBAN MISHRA <anirbanmishra7005@gmail.com>
2023-05-26 16:42:34 +0530

    Backend Authentication Completed

--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,2 +1,8 @@
 export const CART_ADD_ITEM = "CART_ADD_ITEM";
 export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
+
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:anirban-629/Ecommerce-site.git
+git push -u origin main
